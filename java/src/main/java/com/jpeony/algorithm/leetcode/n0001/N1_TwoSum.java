6c5281ab765fd92e25b7ef4199d7a5e2b8445261
yihonglei <jpeony@163.com>
2021-12-12 13:57:25 +0800

    1.Two Sum

--- java/src/main/java/com/jpeony/algorithm/leetcode/n0001/N1_TwoSum.java
+++ java/src/main/java/com/jpeony/algorithm/leetcode/n0001/N1_TwoSum.java
@@ -14,7 +14,7 @@ public class N1_TwoSum {
      * 时间复杂度：根据循环次数，关注循环代码，所以时间复杂度为 O(n)
      * 空间复杂度：每次循环的值都加入到内存中，所以空间复杂度为 O(n)
      */
-    private static int[] twoSum2(int[] nums, int target) {
+    private static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int x = target - nums[i];
@@ -31,7 +31,7 @@ public class N1_TwoSum {
         int[] nums = {2, 7, 11, 15};
         int target = 9;
         // 哈希表
-        int[] resTwo = twoSum2(nums, target);
+        int[] resTwo = twoSum(nums, target);
         System.out.println("哈希表：" + Arrays.toString(resTwo));
     }
 }
