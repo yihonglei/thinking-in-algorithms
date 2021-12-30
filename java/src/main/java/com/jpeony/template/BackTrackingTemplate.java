f61220405f6c606096dd45e2f389db3bc805d544
yihonglei <jpeony@163.com>
2021-12-30 18:40:12 +0800

    BackTracking Template

--- java/src/main/java/com/jpeony/template/BackTrackingTemplate.java
+++ java/src/main/java/com/jpeony/template/BackTrackingTemplate.java
@@ -1,7 +1,15 @@
-package com.jpeony.template;/**
- * 
+package com.jpeony.template;
+
+/**
+ * 【回溯算法模板】
+ *
  * @author yihonglei
- * @date 2021-12-30 18:36:14
  */
 public class BackTrackingTemplate {
+    // 递归终止条件
+    // for 循环
+    // 递归
+    // 想象成树结构
+    // 处理节点撤销
+    // 返回结果
 }
