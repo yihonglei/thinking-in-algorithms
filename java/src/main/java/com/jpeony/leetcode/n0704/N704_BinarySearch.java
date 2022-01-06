1fde8d467f19915f9a156de4e1cbaf9089cb8651
yihonglei <jpeony@163.com>
2022-01-06 17:18:24 +0800

    704. Binary Search

--- java/src/main/java/com/jpeony/leetcode/n0704/N704_BinarySearch.java
+++ java/src/main/java/com/jpeony/leetcode/n0704/N704_BinarySearch.java
@@ -8,9 +8,30 @@ package com.jpeony.leetcode.n0704;
  */
 public class N704_BinarySearch {
 
+    /**
+     * 【二分查找】
+     * 时间复杂度：O(log n)。二分查找每次都是二分治折半查找，所以时间复杂度为 O(log n)。
+     * 空间复杂度：O(1)。只是用了临时变量 mid，所以空间复杂度为 O(1)。
+     */
     private static int search(int[] nums, int target) {
-
-        return 0;
+        // 低位
+        int low = 0;
+        // 高位
+        int high = nums.length - 1;
+        // 注意，这里是 low <= high，不是 low < high
+        while (low <= high) {
+            // 中间值
+            int mid = low + (high - low) / 2;
+            // 中间值等于目标值
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {// 中间值大于目标值，左侧查找
+                high = mid - 1;
+            } else {// 中间值小于目标值，右侧查找
+                low = mid + 1;
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
