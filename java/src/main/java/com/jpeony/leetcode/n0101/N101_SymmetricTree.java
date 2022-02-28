a32e3247cffe38a77584a074ba0f5fb9a4b7c480
yihonglei <jpeony@163.com>
2022-02-28 14:05:05 +0800

    101. Symmetric Tree

---
+++ java/src/main/java/com/jpeony/leetcode/n0101/N101_SymmetricTree.java
@@ -0,0 +1,78 @@
+package com.jpeony.leetcode.n0101;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * [101. Symmetric Tree](https://leetcode.com/problems/symmetric-tree/)
+ * [101. 对称二叉树](https://leetcode-cn.com/problems/symmetric-tree/)
+ *
+ * @author yihonglei
+ */
+public class N101_SymmetricTree {
+
+    /**
+     * 【迭代】
+     * 时间复杂度：O(n)。每个点进队出队各一次。
+     * 空间复杂度：O(n)。队列元素不超过 n 个。
+     */
+    public static boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+
+        queue.offer(root);
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode l = queue.poll();
+            TreeNode r = queue.poll();
+
+            if (l == null && r == null) {
+                continue;
+            }
+
+            if ((l == null || r == null) || (l.val != r.val)) {
+                return false;
+            }
+
+            queue.offer(l.left);
+            queue.offer(r.right);
+
+            queue.offer(l.right);
+            queue.offer(r.left);
+        }
+
+        return true;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(3);
+
+        root.right = node1;
+        node1.left = node2;
+
+        boolean isSymmetric = isSymmetric(root);
+        System.out.println("isSymmetric = " + isSymmetric);
+    }
+}
---
+++ java/src/main/java/com/jpeony/leetcode/n0101/README.md
@@ -0,0 +1,47 @@
+# [101. Symmetric Tree](https://leetcode.com/problems/symmetric-tree/)
+
+## 题目
+
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+Example 1:
+```
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+```
+
+Example 2:
+```
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+```
+
+Example 3:
+```
+Input: root = []
+Output: []
+```
+
+Constraints:
+- The number of nodes in the tree is in the range [1, 1000].
+- -100 <= Node.val <= 100
+
+**Follow up:** Could you solve it both recursively and iteratively?
+
+## 题目含义
+
+检测二叉树节点是否轴对称。
+
+## 算法思路
+
+【迭代】
+
+1、用队列存储 k 层级元素，元素存放顺序为 左的左，右的右，左的右，右的左；
+
+2、每次出队两个元素对比，不相等，则不是轴对称，返回 false，一次迭代完树中的所有节点；
+
+## 复杂度分析
+
+时间复杂度：O(n)。每个点进队出队各一次。
+
+空间复杂度：O(n)。队列元素不超过 n 个。
