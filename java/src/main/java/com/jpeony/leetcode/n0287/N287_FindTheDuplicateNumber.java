9962e5e1874e226ed9c6c4bc2f5cd09155824eeb
yihonglei <jpeony@163.com>
2022-03-29 14:26:19 +0800

    287. Find the Duplicate Number

--- java/src/main/java/com/jpeony/leetcode/n0287/N287_FindTheDuplicateNumber.java
+++ java/src/main/java/com/jpeony/leetcode/n0287/N287_FindTheDuplicateNumber.java
@@ -1,5 +1,7 @@
 package com.jpeony.leetcode.n0287;
 
+import java.util.HashMap;
+
 /**
  * [287. Find the Duplicate Number](https://leetcode.com/problems/find-the-duplicate-number/)
  * [287. 寻找重复数](https://leetcode-cn.com/problems/find-the-duplicate-number/)
@@ -8,6 +10,10 @@ package com.jpeony.leetcode.n0287;
  */
 public class N287_FindTheDuplicateNumber {
 
+    /**
+     * 时间复杂度：O(n)。n 为数组长度。
+     * 空间复杂度：O(1)。只需要临时变量空间。
+     */
     public static int findDuplicate(int[] nums) {
         int slow = 0;
         int fast = 0;
@@ -25,6 +31,22 @@ public class N287_FindTheDuplicateNumber {
         }
         return pre1;
     }
+
+    /**
+     * 时间复杂度：O(n)。n 为数组长度。
+     * 空间复杂度：O(n)。n 为数组长度，HashMap 存储元素个数。
+     */
+    public static int findDuplicateHashMap(int[] nums) {
+        HashMap<Integer, Integer> maps = new HashMap<>();
+        for (int i : nums) {
+            if (maps.get(i) != null) {
+                return i;
+            }
+            maps.put(i, i);
+        }
+        return 0;
+    }
+
 
     public static void main(String[] args) {
         int[] nums = {1, 3, 4, 2, 2};
