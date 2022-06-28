9cb82cfbe1fb5da1baf359de84204b7e2c32de6c
yihonglei <jpeony@163.com>
2022-06-28 18:49:18 +0800

    Container With Most Water

--- java/src/main/java/com/jpeony/leetcode/n0011/N11_ContainerWithMostWater.java
+++ java/src/main/java/com/jpeony/leetcode/n0011/N11_ContainerWithMostWater.java
@@ -10,27 +10,28 @@ public class N11_ContainerWithMostWater {
 
     /**
      * 【双指针】
-     * 时间复杂度：O(n)。整段代码占用时间最多的是 for 循环，循环了 n 次，n 为数组的长度，
-     * 所以时间复杂度是 O(n)。
-     * 空间复杂度：O(1)。整个算法计算过程中，只用了一个存储临时最大值的变量空间，
-     * 所以空间复杂度是 O(1)。
+     * 双指针夹逼，左右指针分别指向数组两边，以较小变构成矩形的高，计算容器面积，
+     * 然后较小边向较大边逼近，计算新的容器面积，保留最大面积，直到左右指针重合，退出夹逼，计算完成。
+     * <p>
+     * 时间复杂度：O(n)。整段代码占用时间最多的是 for 循环，循环了 n 次，n 为数组的长度，所以时间复杂度是 O(n)。
+     * 空间复杂度：O(1)。整个算法计算过程中，只用了一个存储临时最大值的变量空间，所以空间复杂度是 O(1)。
      */
     private static int maxArea(int[] height) {
         // 左右指针
-        int leftPoint = 0, rightPoint = height.length - 1;
+        int left = 0, right = height.length - 1;
         // 最大面积
         int maxArea = 0;
-        while (leftPoint < rightPoint) {
+        while (left < right) {
             // 宽：右指针位置与左指针位置横坐标之差距离
-            int width = rightPoint - leftPoint;
+            int width = right - left;
             // 高：右指针位置与左指针位置纵坐标最小值，只有取最小值才能与另外一边连线构成矩形
             int high = 0;
-            if (height[leftPoint] <= height[rightPoint]) {
-                high = height[leftPoint];
-                leftPoint++;
+            if (height[left] <= height[right]) {
+                high = height[left];
+                right++;
             } else {
-                high = height[rightPoint];
-                rightPoint--;
+                high = height[right];
+                right--;
             }
             // 当前矩阵面积
             int curArea = width * high;
