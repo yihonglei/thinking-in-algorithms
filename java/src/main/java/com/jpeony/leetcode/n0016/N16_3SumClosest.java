9ac912ab3609eabbc74bdf20a6ea2037a05afabf
yihonglei <jpeony@163.com>
2022-07-18 13:46:51 +0800

    16. 3Sum Closest

--- java/src/main/java/com/jpeony/leetcode/n0016/N16_3SumClosest.java
+++ java/src/main/java/com/jpeony/leetcode/n0016/N16_3SumClosest.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 public class N16_3SumClosest {
 
     /**
-     * 【双指针 + 排序】
+     * 【排序 + 双指针】
      * 时间复杂度：O(n^2)。内外两层循环，n * n 为 n^2 次执行，所以时间复杂度为 O(n^2)。
      * 空间复杂度：O(1)。增量空间为临时变量 closestNum，没有别的渐进增长空间，所以空间复杂度为 O(1)。
      */
@@ -29,19 +29,17 @@ public class N16_3SumClosest {
             int rightPoint = n - 1;
             while (leftPoint < rightPoint) {
                 int sum = nums[i] + nums[leftPoint] + nums[rightPoint];
-                // 如果等于 target，必然最接近
-                if (sum == target) {
-                    return target;
-                }
                 // 寻找最接近 target 的三元和
                 if (Math.abs(sum - target) < Math.abs(closestNum - target)) {
                     closestNum = sum;
                 }
-                // 如果大了，尝试从右边往左边夹逼，有没有更好的解
-                if (sum > target) {
+
+                if (sum > target) {// 如果大了，尝试从右边往左边夹逼，有没有更好的解
                     rightPoint--;
-                } else {// 如果小了，尝试从左边往右边夹逼，有没有更好的解
+                } else if (sum < target) {// 如果小了，尝试从左边往右边夹逼，有没有更好的解
                     leftPoint++;
+                } else { // 如果等于 target，必然最接近
+                    return sum;
                 }
             }
         }
