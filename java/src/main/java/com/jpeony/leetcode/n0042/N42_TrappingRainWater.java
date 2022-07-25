bf1d772ce2e1fb770bdd50c8d06cba87e935d9f2
yihonglei <jpeony@163.com>
2022-07-25 13:57:32 +0800

    42. Trapping Rain Water

--- java/src/main/java/com/jpeony/leetcode/n0042/N42_TrappingRainWater.java
+++ java/src/main/java/com/jpeony/leetcode/n0042/N42_TrappingRainWater.java
@@ -15,33 +15,33 @@ public class N42_TrappingRainWater {
      */
     private static int trap(int[] height) {
         // 定义双指针
-        int leftPoint = 0, rightPoint = height.length - 1;
+        int left = 0, right = height.length - 1;
         // 返回结果
         int ans = 0;
         // 构成凹槽左边柱子最大高度，右边柱子最大高度
         int leftMax = 0, rightMax = 0;
-        while (leftPoint < rightPoint) {
+        while (left < right) {
             // 当左边柱子小于右边柱子高度时，右边柱子构成凹槽的高度是可信的，只需要移动左边计算凹槽深度
-            if (height[leftPoint] < height[rightPoint]) {
+            if (height[left] < height[right]) {
                 // 覆盖左边最大值
-                if (height[leftPoint] >= leftMax) {
-                    leftMax = height[leftPoint];
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
                 } else {
                     // 计算凹槽往下凹的深度，即左边最大值减掉当前高度，即为凹下去的深度，这个时候一定要想象到右柱子是右侧远方"移花接木"过来的。
-                    ans += (leftMax - height[leftPoint]);
+                    ans += (leftMax - height[left]);
                 }
                 // 继续往后探寻左边最大值或凹槽深度
-                leftPoint++;
+                left++;
             } else {// 当左边柱子大于右边柱子高度时，左边柱子构成凹槽的高度是可信的，只需要移动右边计算凹槽深度
                 // 覆盖右边最大值
-                if (height[rightPoint] >= rightMax) {
-                    rightMax = height[rightPoint];
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
                 } else {
                     // 计算凹槽往下凹的深度，即右边最大值减掉当前高度，即为凹下去的深度，这个时候一定要想象到左柱子是左侧远方"移花接木"过来的。
-                    ans += (rightMax - height[rightPoint]);
+                    ans += (rightMax - height[right]);
                 }
                 // 继续往前探寻右边最大值或凹槽深度
-                rightPoint--;
+                right--;
             }
         }
 
