b79f4806b7eba4cd2ef27250657383943e4e0d90
yihonglei <jpeony@163.com>
2022-11-03 19:19:25 +0800

    rename

--- java/src/main/java/com/jpeony/leetcode/n0027/N27_RemoveElement.java
+++ java/src/main/java/com/jpeony/leetcode/n0027/N27_RemoveElement.java
@@ -17,14 +17,14 @@ public class N27_RemoveElement {
         if (nums == null || nums.length == 0) {
             return 0;
         }
-        int left = 0, right = 0;
-        while (right < nums.length) {
-            if (nums[right] != val) {
-                nums[left++] = nums[right];
+        int slow = 0, fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
+                nums[slow++] = nums[fast];
             }
-            right++;
+            fast++;
         }
-        return left;
+        return slow;
     }
 
     public static void main(String[] args) {
