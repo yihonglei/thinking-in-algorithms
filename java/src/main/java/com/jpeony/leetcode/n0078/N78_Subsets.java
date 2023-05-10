d1bbe75ce030dd35712bd6577ec442b655991d92
yihonglei <jpeony@163.com>
2023-05-10 16:08:23 +0800

    78. 子集

--- java/src/main/java/com/jpeony/leetcode/n0078/N78_Subsets.java
+++ java/src/main/java/com/jpeony/leetcode/n0078/N78_Subsets.java
@@ -11,29 +11,24 @@ import java.util.List;
  */
 public class N78_Subsets {
 
+    static List<List<Integer>> result = new ArrayList<>();
+    static List<Integer> temp = new ArrayList<>();
+
     /**
      * 【回溯算法】
      * 时间复杂度：O(n×2^n)。一共 2^n 个状态，每种状态需要 O(n) 的时间来构造子集。
      * 空间复杂度：O(n)。渐进空间为存储子集的临时列表，n 为子集个数，所以空间复杂度为 O(n)。
      */
     private static List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        backTracking(0, nums, ans, new ArrayList<>());
-        // 返回结果
-        return ans;
+        backTracking(nums, 0);
+        return result;
     }
 
-    private static void backTracking(int i, int[] nums, List<List<Integer>> ans, ArrayList<Integer> temp) {
-        // 每个子集放入返回结果
-        ans.add(new ArrayList<>(temp));
-        System.out.println("i = " + i + ", ans = " + ans + ", cur = " + temp);
-        // 寻找符合条件的子集合
-        for (int j = i; j < nums.length; j++) {
-            temp.add(nums[j]);
-            // 递归
-            backTracking(j + 1, nums, ans, temp);
-            System.out.println("backtrack-before = " + ans);
-            // 撤销处理结点，回溯过程
+    private static void backTracking(int[] nums, int startIndex) {
+        result.add(new ArrayList<>(temp));
+        for (int i = startIndex; i < nums.length; i++) {
+            temp.add(nums[i]);
+            backTracking(nums, i + 1);
             temp.remove(temp.size() - 1);
         }
     }
