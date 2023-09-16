75169b1ce4c2d9637d95f0bb9b14e878ade60e21
yihonglei <jpeony@163.com>
2023-09-16 22:34:22 +0800

    151. 翻转字符串里的单词

--- java/src/main/java/com/jpeony/leetcode/n0151/N151_ReverseWordsInAString.java
+++ java/src/main/java/com/jpeony/leetcode/n0151/N151_ReverseWordsInAString.java
@@ -39,8 +39,24 @@ public class N151_ReverseWordsInAString {
     }
 
     private static String reverseWordsTwoPoint(String s) {
+        // 去除前后空格
+        s.trim();
 
-        return null;
+        int left = s.length() - 1;
+        int right = left;
+        StringBuilder sb = new StringBuilder();
+        while (left >= 0) {
+            // 左指针向左一直走，直到遇到第一个空格
+            while (left >= 0 && s.charAt(left) !=' ') left--;
+            // 截取单词
+            sb.append(s.substring(left + 1, right + 1)).append(" ");
+            // 左指针继续向左走，刨掉空格，直到遇到第一个非空格
+            while (left >= 0 && s.charAt(left) == ' ') left--;
+            // 挪动右指针，到下一单词的末尾位置
+            right = left;
+        }
+
+        return sb.toString().trim();
     }
 
     public static void main(String[] args) {
