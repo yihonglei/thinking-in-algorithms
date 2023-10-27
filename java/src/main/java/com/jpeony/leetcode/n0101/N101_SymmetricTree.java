f0ca9c07bc26fb74a662fcf25dc93595647268ab
yihonglei <jpeony@163.com>
2023-10-27 17:59:30 +0800

    101. 对称二叉树

--- java/src/main/java/com/jpeony/leetcode/n0101/N101_SymmetricTree.java
+++ java/src/main/java/com/jpeony/leetcode/n0101/N101_SymmetricTree.java
@@ -44,6 +44,37 @@ public class N101_SymmetricTree {
         return true;
     }
 
+    /**
+     * 【递归】
+     */
+    public static boolean isSymmetricR(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        //调用递归函数，比较左节点，右节点
+        return dfs(root.left, root.right);
+    }
+
+    public static boolean dfs(TreeNode left, TreeNode right) {
+        //递归的终止条件是两个节点都为空
+        //或者两个节点中有一个为空
+        //或者两个节点的值不相等
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+        //再递归的比较 左节点的左孩子 和 右节点的右孩子
+        //以及比较  左节点的右孩子 和 右节点的左孩子
+        return dfs(left.left, right.right) && dfs(left.right, right.left);
+    }
+
     private static class TreeNode {
         int val;
         TreeNode left;
