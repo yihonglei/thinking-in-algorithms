dd5c408d45e5c42528f994f5df4b6f059019cef6
yihonglei <jpeony@163.com>
2023-12-06 10:53:48 +0800

    108. 将有序数组转换为二叉搜索树

---
+++ java/src/main/java/com/jpeony/base/common/TreeNode.java
@@ -0,0 +1,23 @@
+package com.jpeony.base.common;
+
+/**
+ * 树节点 公共代码
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
