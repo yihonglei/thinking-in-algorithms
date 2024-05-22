085a9b18d6ade2c5e5057b35bf8a92c2a727ff42
yihonglei <jpeony@163.com>
2024-05-22 16:31:47 +0800

    AddDigits

---
+++ java/src/main/java/com/jpeony/leetcode/n0258/N258_AddDigits.java
@@ -0,0 +1,44 @@
+package com.jpeony.leetcode.n0258;
+
+public class N258_AddDigits {
+
+    public static int addDigits1(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+
+        return num;
+    }
+
+
+    // 方法2：递归
+    public static int addDigits2(int num) {
+        if (num < 10) {
+            return num;
+        }
+        return r(num);
+    }
+
+    public static int r(int num) {
+        if (num < 10) {
+            return num;
+        }
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num = num / 10;
+        }
+        num = sum;
+        return r(num);
+    }
+
+    public static void main(String[] args) {
+        int result = addDigits1(38);
+        System.out.println(result);
+    }
+}
