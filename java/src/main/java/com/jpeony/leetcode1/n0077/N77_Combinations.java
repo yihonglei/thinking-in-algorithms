bf02efdc15446babddd753e1010be2ba695e7b9e
yihonglei <jpeony@163.com>
2024-08-19 10:33:12 +0800

    77. 组合

--- java/src/main/java/com/jpeony/leetcode1/n0077/N77_Combinations.java
+++ java/src/main/java/com/jpeony/leetcode1/n0077/N77_Combinations.java
@@ -9,18 +9,16 @@ import java.util.List;
  */
 public class N77_Combinations {
     public static List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> ans = new ArrayList<>(); // 全部结果收集
-        backtracking(1, n, k, new LinkedList<Integer>(), ans); // 单次结果收集
+        List<List<Integer>> ans = new ArrayList<>();
+        backtracking(1, n, k, new LinkedList<Integer>(), ans);
         return ans;
     }
 
-    public static void backtracking(int startIndex, int n, int k,
-                                    LinkedList<Integer> path, List<List<Integer>> ans) {
+    public static void backtracking(int startIndex, int n, int k, LinkedList<Integer> path, List<List<Integer>> ans) {
         if (path.size() == k) {
             ans.add(new ArrayList<>(path));
             return;
         }
-
         for (int i = startIndex; i <= n; i++) {
             path.add(i);
             backtracking(i + 1, n, k, path, ans);
