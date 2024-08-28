87089de54b06eca0a6aebdf8c7bcd9cc0c55ce57
yihonglei <jpeony@163.com>
2024-08-28 18:11:42 +0800

    78. 子集

--- java/src/main/java/com/jpeony/leetcode1/n0078/N78_Subsets.java
+++ java/src/main/java/com/jpeony/leetcode1/n0078/N78_Subsets.java
@@ -1,6 +1,7 @@
 package com.jpeony.leetcode1.n0078;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -10,26 +11,30 @@ import java.util.List;
  * @author yihonglei
  */
 public class N78_Subsets {
-
-    static List<List<Integer>> result = new ArrayList<>();
-    static List<Integer> temp = new ArrayList<>();
-
     /**
      * 【回溯算法】
      * 时间复杂度：O(n×2^n)。一共 2^n 个状态，每种状态需要 O(n) 的时间来构造子集。
      * 空间复杂度：O(n)。渐进空间为存储子集的临时列表，n 为子集个数，所以空间复杂度为 O(n)。
      */
     private static List<List<Integer>> subsets(int[] nums) {
-        backTracking(nums, 0);
-        return result;
+        List<List<Integer>> ans = new ArrayList<>();
+        LinkedList<Integer> path = new LinkedList<>();
+        backtracking(ans, path, nums, 0);
+        return ans;
     }
 
-    private static void backTracking(int[] nums, int startIndex) {
-        result.add(new ArrayList<>(temp));
+    private static void backtracking(List<List<Integer>> ans, LinkedList<Integer> path, int[] nums, int startIndex) {
+        ans.add(new ArrayList<>(path));
+        if (startIndex >= nums.length) {
+            return;
+        }
         for (int i = startIndex; i < nums.length; i++) {
-            temp.add(nums[i]);
-            backTracking(nums, i + 1);
-            temp.remove(temp.size() - 1);
+            path.add(nums[i]);
+            System.out.println("cur：i=" + i + " path = " + path.toString());
+            backtracking(ans, path, nums, i + 1);
+            System.out.println("remove before：i=" + i + " path = " + path.toString());
+            path.remove(path.size() - 1);
+            System.out.println("remove after ：i=" + i + " path = " + path.toString());
         }
     }
 
