ae512c04274745f7774d5aa24bbfe3c76a9c31eb
yihonglei <jpeony@163.com>
2024-09-25 16:09:08 +0800

    435. 无重叠区间

---
+++ java/src/main/java/com/jpeony/leetcode1/n0435/N435_NonOverLappingIntervals.java
@@ -0,0 +1,15 @@
+package com.jpeony.leetcode1.n0435;
+
+/**
+ * [435. 无重叠区间](https://leetcode.cn/problems/non-overlapping-intervals/)
+ */
+public class N435_NonOverlapingIntervals {
+    public int eraseOverlapIntervals(int[][] intervals) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
