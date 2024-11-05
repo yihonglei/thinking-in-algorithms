4032cb3b78b6756d66ed5c8310e2dc09c88b2831
yihonglei <jpeony@163.com>
2024-11-05 15:47:12 +0800

    746. 使用最小花费爬楼梯

--- java/src/main/java/com/jpeony/leetcode1/n0746/N746_MinCostClimbingStairs.java
+++ java/src/main/java/com/jpeony/leetcode1/n0746/N746_MinCostClimbingStairs.java
@@ -4,12 +4,20 @@ package com.jpeony.leetcode1.n0746;
  * [746. 使用最小花费爬楼梯](https://leetcode.cn/problems/min-cost-climbing-stairs)
  */
 public class N746_MinCostClimbingStairs {
+    /**
+     * 算法：动态规划
+     * 时间复杂度：O(n)。n 为楼梯阶数。
+     * 空间复杂：O(n)。n 为楼梯阶数。
+     */
     public static int minCostClimbingStairs(int[] cost) {
         int len = cost.length;
-        int[] dp = new int[len + 1]; // 第i阶最小的花费
+        // 确定dp数组以及下标的含义：第i阶最小的花费为dp[i]
+        int[] dp = new int[len + 1];
+        // 初始化dp数组
         dp[0] = 0;
         dp[1] = 0;
-        for (int i = 2; i <= len; i++) {
+        for (int i = 2; i <= len; i++) {// 确定遍历顺序：从前到后
+            // 确定递推公式：dp[i] = Math.min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
             dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
         }
 
