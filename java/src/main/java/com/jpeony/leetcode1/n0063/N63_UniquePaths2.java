3b137b9b88d0a72dc6c629cb17f5cc265b2f4c81
yihonglei <jpeony@163.com>
2024-11-07 16:09:51 +0800

    63. 不同路径 II

--- java/src/main/java/com/jpeony/leetcode1/n0063/N63_UniquePaths2.java
+++ java/src/main/java/com/jpeony/leetcode1/n0063/N63_UniquePaths2.java
@@ -9,24 +9,27 @@ public class N63_UniquePaths2 {
         int n = obstacleGrid[0].length;
         int[][] dp = new int[m][n];
 
-        // 如果在起点或终点出现了障碍，直接返回0
-        if (obstacleGrid[m - 1][n - 1] == 1 || obstacleGrid[0][0] == 1) {
+        // 如果起点或终点出现了障碍，直接返归 0
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) {
             return 0;
         }
 
         for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) {
             dp[i][0] = 1;
         }
+
         for (int j = 0; j < n && obstacleGrid[0][j] == 0; j++) {
             dp[0][j] = 1;
         }
 
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                dp[i][j] = (obstacleGrid[i][j] == 0) ? dp[i - 1][j] + dp[i][j - 1] : 0;
+                dp[i][j] = (obstacleGrid[i][j] == 0) ? dp[i -1][j] + dp[i][j -1] : 0;
             }
         }
-        return dp[m - 1][n - 1];
+
+
+        return dp[m-1][n-1];
     }
 
     public static void main(String[] args) {
