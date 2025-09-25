22ebb8bb75eb54640ddb6f266510a7a102a49ae9
yihonglei <jpeony@163.com>
2025-09-25 18:43:31 +0800

    common

--- c/src/leetcode1/n0027/N27_RemoveElement.c
+++ c/src/leetcode1/n0027/N27_RemoveElement.c
@@ -1,5 +1,8 @@
 #include "N27_RemoveElement.h"
 
+/**
+ * [27. 移除元素](https://leetcode.cn/problems/remove-element/)
+ */
 int removeElement(int *nums, int numsSize, int val) {
     int slow = 0;
     for (int fast = 0; fast < numsSize; fast++) {
