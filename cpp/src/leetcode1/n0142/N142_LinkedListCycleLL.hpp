4d878b26656c078dcf13a0c7b88721259ea33a44
yihonglei <jpeony@163.com>
2026-05-29 11:22:57 +0800

    common

--- cpp/src/leetcode1/n0142/N142_LinkedListCycleLL.hpp
+++ cpp/src/leetcode1/n0142/N142_LinkedListCycleLL.hpp
@@ -6,11 +6,16 @@
 #define CPP_N142_LINKEDLISTCYCLELL_HPP
 
 
-
 class N142_LinkedListCycleLL {
+public:
+    struct ListNode {
+        int val;
+        ListNode* next;
+        ListNode(int val):val(val),next(nullptr){}
+    };
 
+    ListNode *detectCycle(ListNode *head);
 };
-
 
 
 #endif //CPP_N142_LINKEDLISTCYCLELL_HPP
